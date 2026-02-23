920b0759d9840199832ea86cbc26728d31f35de1
Abdulbaki Cakir <abcakir@stud.hs-heilbronn.de>
2026-02-23 21:13:11 +0100

    implement login + jwt

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
 
+from app import crud
+from app.core import security
+from app.core.security import create_access_token
 from app.api.deps import get_db
 from app.schemas.user import UserCreate, UserResponse
 from app.crud.user import create_user, get_user_by_email
@@ -21,3 +25,14 @@ def register(user_in: UserCreate, db: Session = Depends(get_db)):
     new_user = create_user(db=db, user=user_in)
 
     return new_user
+
+@router.post("/login")
+def login(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
+    user = crud.user.get_user_by_email(db, email=form_data.username)
+    if not user:
+        raise HTTPException(status_code=400, detail="E-Mail oder Passwort falsch.")
+    if not security.verify_password(form_data.password, user.password_hash):
+        raise HTTPException(status_code=400, detail="E-Mail oder Passwort falsch.")
+    acess_token = create_access_token(subject=user.id)
+
+    return {"acess_token": acess_token, "token_type": "bearer"}
--- app/core/config.py
+++ app/core/config.py
@@ -9,6 +9,10 @@ class Settings(BaseSettings):
     POSTGRES_PORT: str
     POSTGRES_DB: str
 
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
     @property
     def DATABASE_URL(self) -> str:
         return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
