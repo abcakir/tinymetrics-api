ac2dcdbf4bfa89c6c7c7993ab680e6640f67ffcf
Abdulbaki Cakir <abcakir@stud.hs-heilbronn.de>
2026-02-25 20:57:30 +0100

    fix dockercompose

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://tinyuser:tinypassword@localhost:5432/tinymetrics"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://tinyuser:tinypassword@localhost:5432/tinymetrics")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
